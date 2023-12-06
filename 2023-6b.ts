3f2385235eccd6a12e386cb9a269cafe0961b591
kosciolek <45483493+kosciolek@users.noreply.github.com>
2023-12-06 15:49:46 +0100

    2023-6

--- 2023-6b.ts
+++ 2023-6b.ts
@@ -1,12 +1,6 @@
-const getDigits = (string: string) =>
-  string
-    .split("")
-    .filter((char) => char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57)
-    .join("");
-
 const [timeLine, distanceLine] = Deno.readTextFileSync("2023-6.txt").split("\n");
-const time = Number(getDigits(timeLine));
-const distance = Number(getDigits(distanceLine));
+const time = Number(timeLine.replace(/[^\d]/g, ""));
+const distance = Number(distanceLine.replace(/[^\d]/g, ""));
 
 const delta = time ** 2 - 4 * 1 * distance;
 // CHARGE_TIME^2 - CHARGE_TIME * TOTAL_TIME + BEST_DISTANCE = 0
