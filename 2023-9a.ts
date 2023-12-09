d532e225ca6c60fe97755038ea152b66ca57a6b0
kosciolek <45483493+kosciolek@users.noreply.github.com>
2023-12-09 19:12:10 +0100

    2023-9

---
+++ 2023-9a.ts
@@ -0,0 +1,29 @@
+const answer = Deno.readTextFileSync("2023-9.txt")
+  .split("\n")
+  .map((line) => line.split(" ").map((reading) => Number(reading)))
+  .map((readings) => {
+    const triangle = [readings];
+    let requiresNextSequence = true;
+    while (requiresNextSequence) {
+      requiresNextSequence = false;
+      const currentSequence = triangle[triangle.length - 1];
+      const newSequence: number[] = [];
+      for (let i = 1; i < currentSequence.length; i += 1) {
+        const diff = currentSequence[i] - currentSequence[i - 1];
+        newSequence.push(diff);
+        if (diff !== 0) {
+          requiresNextSequence = true;
+        }
+      }
+      triangle.push(newSequence);
+    }
+    let prediction = 0;
+    for (let i = triangle.length - 2; i >= 0; i -= 1) {
+      const sequenceLength = triangle[i].length;
+      prediction += triangle[i][sequenceLength - 1];
+    }
+    return prediction;
+  })
+  .reduce((sum, current) => sum + current);
+
+console.log(answer);
